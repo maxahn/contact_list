c02fe8a252d97efc1f3a694b181efb0c065969fb
maxahn <ahnmaximus@gmail.com>
2016-05-31 14:36:42 -0700

    finished writing version1 of tests

--- tests.rb
+++ tests.rb
@@ -54,37 +54,63 @@ describe Contact do
 
   describe '#find' do
     it 'should return contact in .csv file with matching id' do
-      found_contact = find(123)
+      found_contact = Contact.find(123)
       expect(found_contact).to eq(@maxwell)
     end
    
     it 'should return another contact in .csv file with matching id' do
-      found_contact = find (332)
+      found_contact = Contact.find(332)
       expect(found_contact).to eq(@rocky)
     end
 
     it 'should return nil if .csv file does not contain match with id' do 
-      found_contact = find (234234234)
+      found_contact = Contact.find(234234234)
       expect(found_contact).to eq(nil)
     end
   end
 
   describe '#search' do
 
+    before :each do 
+      @maxwell = Contact.new('Maxwell', 'maxwell@awesome.com', 123)
+      @jade    = Contact.new('Jade', 'bunnypower@sleepy.com', 2333)
+      @rocky   = Contact.new('Rocky','perogies@village.com', 332)
+      @maximus = Contact.new('Maximus', 'sheeppower@sleepy.com', 144) 
+    end
+    
     it 'should search by email[full] and return an array of matching contacts' do
-       
+       found_contacts = Contact.search('maxwell@awesome.com')
+       expect(found_contacts).to eq([@maxwell])
     end
 
-    it 'should search by name[full] and return an array of matching contacts' do 
+    it 'should search by name[full] and return an array of matching contacts' do
+      found_contacts = Contact.search('Jade')
+      expect(found_contacts).to eq([@jade])
     end
 
     it 'should search by email[partial] and return an array of matching contacts' do
+      found_contacts = Contact('@village.com')
+      expect(found_contacts).to eq([@rocky])
     end
-    
+
+    it 'should return all the contacts containing the search term (email)' do
+      found_contacts = Contact.search('sleepy')
+      expect(found_contacts).to eq([@jade, @maximus])
+    end 
+  
+    it 'should return all the contacts containing the search term (name)' do
+      found_contacts = Contact.search('Max')
+      expect(found_contacts).to eq([@maxwell, @maximus])
+    end
+
     it 'should search by name[partial] and return an array of matching contacts' do
+      found_contacts = Contact.search('Ro')
+      expect(found_contacts).to eq([@rocky])
     end
     
     it 'should return matching results from both the email and name fields' do
+      found_contacts = Contact.search('a')
+      expect(found_contacts).to eq([@maxwell, @jade, @rocky, @maximus]) 
     end
   end
 end
