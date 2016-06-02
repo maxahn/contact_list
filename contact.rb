74920b9d1a7dc36ad99599c5f840e59cc9486eca
maxahn <ahnmaximus@gmail.com>
2016-06-01 18:03:09 -0700

    FRICKING RSPEC

--- contact.rb
+++ contact.rb
@@ -6,29 +6,38 @@ class Contact
 
   attr_accessor :name, :email, :id
   @@csv_file = './test_contact.csv'
-  @@id_tracker = 0  
-  # Creates a new contact object
+  @@id_tracker = 1  
+  File_Path = @@csv_file
+# Creates a new contact object
     # @param name [String] The contact's name
     # @param email [String] The contact's email address
   def initialize(name, email)
     # TODO: Assign parameter values to instance variables.
     @name = name
     @email = email
-    @id = @@id_tracker 
+    @id = @@id_tracker
     @@id_tracker += 1
   end
 
+     
     # Provides functionality for managing contacts in the csv file.
   class << self #######SO THESE ARE CLASS MEHTODS? DOES THIS MEAN TO USE THEM, YOU WOULD USE CLASS.METHOD INSTEAD OF USING THEM ON A PARTICULAR INSTANCE OF THE CLASS?###### 
     # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
     # @return [Array<Contact>] Array of Contact objects
     def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.    
-#       CSV.foreach(@@csv_file, 'r') do |contact_row|
-#       puts contact_row
-      puts CSV.read(@@csv_file)[0].class
-      puts [['Maxwell', 'maxwell@awesome.com'],['Jade', 'bunnypower@sleepy.com'],['Rocky','perogies@village.com']]
-#       end
+      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.   
+    contact_list = []
+      CSV.foreach(@@csv_file, 'r') do |contact_row|
+        name = contact_row[0]
+        email = contact_row[1]
+        id = contact_row[2]
+        
+        contact = Contact.new(name, email)
+        contact.id = id 
+        contact_list << contact       
+# puts CSV.read(@@csv_file)
+      end
+      contact_list
     end
 
       # Creates a new contact, adding it to the csv file, returning the new contact.
@@ -36,22 +45,52 @@ class Contact
       # @param email [String] the contact's email
     def create(name, email)
       # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+      new_contact = Contact.new(name, email)
+      CSV.open(@@csv_file, 'a') do |csv|
+        csv << [new_contact.name, new_contact.email, new_contact.id]
+      end
+      new_contact
     end
-            
+           
       # Find the Contact in the 'contacts.csv' file with the matching id.
       # @param id [Integer] the contact id
       # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
       # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+
+        contacts = Contact.all
+
+        contacts.each do |contact|
+          if contact.id == id.to_s 
+            return contact
+          end
+        end
+        nil
     end
-            
+
       # Search for contacts by either name or email.
       # @param term [String] the name fragment or email fragment to search for
       # @return [Array<Contact>] Array of Contact objects.
     def search(term)
       # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
-    end
+      #
+      contacts = Contact.all
+      term.downcase!
+      
+      contacts.find_all do |contact|
+        name = contact.name.downcase
+        email = contact.email.downcase
+        #binding.pry
+        name.include?(term) || email.include?(term)
+      end
 
+        
+    end
+        
+    def id_tracker 
+      @@id_tracker
+    end
   end
 
 end
+
--- tests.rb
+++ tests.rb
@@ -1,118 +1,203 @@
 require_relative 'contact'
-require_relative 'contact_list'
+require 'pry'
+require 'csv'
 
 
-describe Contact do
-  @@test_file = CSV.read('./test_contact.csv') 
-    
-  describe '.new' do
-    before :each do 
-      @contact = Contact.new("Maxwell", "maxahn@maxahn.com")
-    end
-    
-    it 'should be able to report its name' do
-      expect(@contact.name).to eq('Maxwell')
-
-    end
-
-    it 'should be able to report its email' do
-      expect(@contact.email).to eq('maxahn@maxahn.com')
-    end
-  end
-
-  describe '#create' do
-    before :each do
-      @name = 'John Doe'
-      @email = 'johndoe@maxahn.com'
-      @new_contact = Contact.create(@name, @email)
-    end
-
-    it 'should return the newly made contact' do
-      expect(Contact.create(@name, @email)).to eq(Contact.new(@name, @email))
-    end
-
-    it 'should increment id number' do 
-      expect(create('a', 'a').id).to eq(1)
-    end 
-    it 'adds newly created contact to the csv file' do
-      expect(@@test_file.include?(['John Doe', 'johndoe@maxahn.com', 0])).to be true 
-    end 
-  end
-
-  describe '#all' do 
-    before :each do 
-      @maxwell = Contact.new('Maxwell', 'maxwell@awesome.com', 123)
-      @jade    = Contact.new('Jade', 'bunnypower@sleepy.com', 2333)
-      @rocky   = Contact.new('Rocky','perogies@village.com', 332)
-    end
-
-    it 'should return an array of all Contact instances made from data in contacts.csv' do
-      expect(Contact.all).to eq([@maxwell,@jade,@rocky])
-    end
- 
-  end
-
-  describe '#find' do
-    it 'should return contact in .csv file with matching id' do
-      found_contact = Contact.find(123)
-      expect(found_contact).to eq(@maxwell)
-    end
-   
-    it 'should return another contact in .csv file with matching id' do
-      found_contact = Contact.find(332)
-      expect(found_contact).to eq(@rocky)
-    end
-
-    it 'should return nil if .csv file does not contain match with id' do 
-      found_contact = Contact.find(234234234)
-      expect(found_contact).to eq(nil)
-    end
-  end
-
-  describe '#search' do
-
-    before :each do 
-      @maxwell = Contact.new('Maxwell', 'maxwell@awesome.com', 123)
-      @jade    = Contact.new('Jade', 'bunnypower@sleepy.com', 2333)
-      @rocky   = Contact.new('Rocky','perogies@village.com', 332)
-      @maximus = Contact.new('Maximus', 'sheeppower@sleepy.com', 144) 
-    end
-    
-    it 'should search by email[full] and return an array of matching contacts' do
-       found_contacts = Contact.search('maxwell@awesome.com')
-       expect(found_contacts).to eq([@maxwell])
-    end
-
-    it 'should search by name[full] and return an array of matching contacts' do
-      found_contacts = Contact.search('Jade')
-      expect(found_contacts).to eq([@jade])
-    end
-
-    it 'should search by email[partial] and return an array of matching contacts' do
-      found_contacts = Contact('@village.com')
-      expect(found_contacts).to eq([@rocky])
-    end
-
-    it 'should return all the contacts containing the search term (email)' do
-      found_contacts = Contact.search('sleepy')
-      expect(found_contacts).to eq([@jade, @maximus])
-    end 
-  
-    it 'should return all the contacts containing the search term (name)' do
-      found_contacts = Contact.search('Max')
-      expect(found_contacts).to eq([@maxwell, @maximus])
-    end
-
-    it 'should search by name[partial] and return an array of matching contacts' do
-      found_contacts = Contact.search('Ro')
-      expect(found_contacts).to eq([@rocky])
-    end
-    
-    it 'should return matching results from both the email and name fields' do
-      found_contacts = Contact.search('a')
-      expect(found_contacts).to eq([@maxwell, @jade, @rocky, @maximus]) 
-    end
-  end
+CSV.open(Contact::File_Path, 'w') do |csv|
+   csv << ['Maxwell', 'maxwell@awesome.com','0']
 end
+
+@test_file = CSV.read(Contact::File_Path)
+
+puts '#CREATE --------------------------'
+
+  puts "Test File, should have ONE element: #{@test_file}" 
+
+  @rocky = Contact.create('Rocky','perogies@village.com')
+  @jade = Contact.create('Jade', 'bunnypower@sleepy.com')
+
+
+  @test_file = CSV.read(Contact::File_Path)
+  puts "Test File, should have THREE elements: #{@test_file}"
+
+puts '#ALL -----------------------------'
+  contacts = Contact.all
+  contacts.each do |contact|
+  puts "Name: #{contact.name}, email: #{contact.email}, id: #{contact.id}"
+  end
+
+puts 'FIND -----------------------------'
+
+puts "Find Rocky by ID #1"
+
+  contact_found = Contact.find(1)
+  puts "Found: #{contact_found.name}"
+  puts "Trying to find non-existing contact with ID # 100"
+  puts "Found: #{Contact.find(100).class}"
+  
+puts 'SEARCH ---------------------------'
+
+  puts "Searching for contacts with the letter 'awesome':"
+  Contact.search("max").each {|contact| puts "Name: #{contact.name}"}
+
+  puts "Searching for contacts with the letter 'A':"
+  Contact.search("a").each {|contact| puts "Name: #{contact.name}"}
+
+  Contact.create("Julie", "sheepPower@sleepy.com")
+
+  puts "Searching for contacts with the letter 'sleePy':"
+  Contact.search("sleePy").each {|contact| puts "Name: #{contact.name}"}
+
+File.delete(Contact::File_Path)                   #to clear file  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#describe Contact do
+
+# @@test_file_path = './test_contact.csv' 
+
+# before :each do 
+
+#   File.delete(Contact::File_Path)                   #to clear file  
+
+#   @name = 'John Doe'
+#   @email = 'johndoe@maxahn.com'
+#   @new_contact = Contact.create(@name, @email)
+
+#   @maxwell = Contact.create('Maxwell', 'maxwell@awesome.com')
+#   @jade = Contact.create('Jade', 'bunnypower@sleepy.com')
+#   @rocky = Contact.create('Rocky','perogies@village.com')
+#   @maximus = Contact.create('Maximus', 'sheeppower@sleepy.com') 
+
+#   @test_file = CSV.read(Contact::File_Path)
+# end
+
+# describe '.new' do
+#   before :each do 
+#     @contact = Contact.new('Maxwell', 'maxahn@maxahn.com')
+#   end
+#   it 'should be able to report its name' do
+#     expect(@contact.name).to eq('Maxwell')
+#   end
+
+#   it 'should be able to report its email' do
+#     expect(@contact.email).to eq('maxahn@maxahn.com')
+#   end
+# end
+
+# describe '#create' do
+#   
+#   it 'should return the newly made contact' do
+#     expect(@new_contact.name).to eq(@name)
+#     expect(@new_contact.email).to eq(@email)
+#   end
+#   it 'should increment id number' do
+#     newer_contact = Contact.create('someone','emaillll')
+#     expect(newer_contact.id).to eq(@maximus.id + 1)
+#   end 
+
+#   it 'adds newly created contact to the csv file' do
+#     expect(@test_file.include?(['John Doe', 'johndoe@maxahn.com', @new_contact.id.to_s])).to be true 
+#   end
+#   
+#   it 'adds multiple contacts' do 
+#     more_contact = Contact.create('Bob', "bob@gmail.com")
+#     expect(@test_file).to include(['John Doe', 'johndoe@maxahn.com', @new_contact.id.to_s])
+#     expect(@test_file).to include(['Bob',"bob@gmail.com", more_contact.id.to_s]) 
+#   end
+
+# end
+
+# describe '#all' do
+
+#   it 'should return an array of all Contact instances made from data in contacts.csv' do
+#     expect(Contact.all).to eq([@maxwell,@jade,@rocky,@maximus])
+#   end
+#
+# end
+
+# describe '#find' do
+#   
+#   it 'should return contact in .csv file with matching id' do
+#     found_contact = Contact.find(123)
+#     expect(found_contact).to eq(@maxwell)
+#   end
+#  
+#   it 'should return another contact in .csv file with matching id' do
+#     found_contact = Contact.find(332)
+#     expect(found_contact).to eq(@rocky)
+#   end
+
+#   it 'should return nil if .csv file does not contain match with id' do 
+#     found_contact = Contact.find(234234234)
+#     expect(found_contact).to eq(nil)
+#   end
+# end
+
+#  describe '#search' do
+
+#   it 'should search by email[full] and return an array of matching contacts' do
+#      found_contacts = Contact.search('maxwell@awesome.com')
+#      expect(found_contacts).to eq([@maxwell])
+#   end
+
+#   it 'should search by name[full] and return an array of matching contacts' do
+#     found_contacts = Contact.search('Jade')
+#     expect(found_contacts).to eq([@jade])
+#   end
+
+#   it 'should search by email[partial] and return an array of matching contacts' do
+#     found_contacts = Contact('@village.com')
+#     expect(found_contacts).to eq([@rocky])
+#   end
+
+#   it 'should return all the contacts containing the search term (email)' do
+#     found_contacts = Contact.search('sleepy')
+#     expect(found_contacts).to eq([@jade, @maximus])
+#   end 
+# 
+#   it 'should return all the contacts containing the search term (name)' do
+#     found_contacts = Contact.search('Max')
+#     expect(found_contacts).to eq([@maxwell, @maximus])
+#   end
+
+#   it 'should search by name[partial] and return an array of matching contacts' do
+#     found_contacts = Contact.search('Ro')
+#     expect(found_contacts).to eq([@rocky])
+#   end
+#   
+#   it 'should return matching results from both the email and name fields' do
+#     found_contacts = Contact.search('a')
+#     expect(found_contacts).to eq([@maxwell, @jade, @rocky, @maximus]) 
+#   end
+#  end
+#end
 
   
